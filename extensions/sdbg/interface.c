3db484bf1d1d675c465e0c49a5dac8566db9679b
AIR <airrsongs@gmail.com>
2018-11-28 14:17:11 -0500

    Enabled the automatic creation of dummy sdbg.bas file

--- extensions/sdbg/interface.c
+++ extensions/sdbg/interface.c
@@ -19,10 +19,36 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 This program implements a simple debugger "preprocessor" for ScriptBasic.
 
+BAS    : sdbg.bas
+
 NTLIBS: user32.lib ws2_32.lib
 UXLIBS:
 DWLIBS:
 MCLIBS:
+
+TO_BAS:
+REM """
+This is dbg.bas
+
+This file is copied into the module header files directory.
+dbg.dll or dbg.so is not a module. It is a preprocessor
+that helps the programmer to debug. There is no need to
+include any header file to use a preprocessor.
+
+However the stupid installer thinks that this 'module' was not
+compiled properly if there is no header file. Therefore here
+this file is here as a placeholder. Do not use it!
+
+Actually I wrote the stupid installer and I decided that it is
+better to have a dummy header file for each preprocessor than
+recognizing a module properly installed when the header file
+is actually corrupt or missing.
+
+Regards,
+Peter Verhas
+September 6, 2002.
+
+"""
 
 */
 #include <stdio.h>
