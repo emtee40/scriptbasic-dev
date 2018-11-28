2488584a3a8cb5132bf76531c1f93444262c6b40
AIR <airrsongs@gmail.com>
2018-11-28 14:39:41 -0500

    Enabled automatic creation of mt.bas module file

--- extensions/mt/interface.c
+++ extensions/mt/interface.c
@@ -23,10 +23,36 @@ You should have received a copy of the GNU Lesser General Public
 License along with this library; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
+  FILE   : interface.c
+
+  BAS    : mt.bas
+
 NTLIBS:
 UXLIBS:
 DWLIBS: -init __init
 
+TO_BAS:
+declare sub ::SetVariable        alias "setmtvariable" lib "mt"
+declare sub ::GetVariable        alias "getmtvariable" lib "mt"
+
+declare sub ::LockWrite          alias "lockmtwrite"   lib "mt"
+declare sub ::UnlockWrite        alias "unlockmtwrite" lib "mt"
+declare sub ::LockRead           alias "lockmtread"    lib "mt"
+declare sub ::UnlockRead         alias "unlockmtread"  lib "mt"
+
+declare sub ::NewSessionId       alias "newsession"    lib "mt"
+declare sub ::DeleteSession      alias "delsession"    lib "mt"
+declare sub ::SetSessionId       alias "setsession"    lib "mt"
+declare sub ::GetSessionId       alias "getsession"    lib "mt"
+declare sub ::CheckSessionId     alias "chksession"    lib "mt"
+declare sub ::SetSessionVariable alias "setsvariable"  lib "mt"
+declare sub ::GetSessionVariable alias "getsvariable"  lib "mt"
+
+declare sub ::SessionTimeout     alias "sessionto"     lib "mt"
+declare sub ::GetSessionTimeout  alias "getsesto"      lib "mt"
+declare sub ::GetSessionPingTime alias "getsesto"      lib "mt"
+declare sub ::SessionCount       alias "sessioncount"  lib "mt"
+declare sub ::ListSessions       alias "listses"       lib "mt"
 */
 #include <stdio.h>
 #include <time.h>
