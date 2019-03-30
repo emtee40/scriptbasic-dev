662d4a931f0eeb3bfb9605caf32ab40d62da7bad
Armando Rivera <air@binarymagic.net>
2019-03-30 02:31:33 -0400

    Updated to output to PACKAGES folder

--- mkappimage.sh
+++ mkappimage.sh
@@ -13,7 +13,7 @@ if [[ -n $DEST ]];then
     cp bin/lib/* "$DEST/usr/lib"
     cp bin/mod/dll/* "$DEST/usr/module"
     find extensions -name "*.bas" -exec cp {} "$DEST/usr/include" \;
-    "$APT" "$DEST" "$PWD/bin/sb.AppImage"
+    "$APT" "$DEST" "$PWD/PACKAGES/sb.AppImage"
     [[ -d "$DEST" ]] && rm -r "$DEST"
 else
     printf "\n**WARNING** Missing path to destination folder\n"
